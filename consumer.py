5a1ea7acfa66a9d2764e28768457981bc7367033
Vincent D <vinc.dep@gmail.com>
2017-11-28 11:42:58 +0100

    Update consumer.py
    Add ssl context

--- consumer.py
+++ consumer.py
@@ -4,7 +4,10 @@ TOPIC_NAME = 'kafkaesque'
 BOOTSTRAP_SERVERS = 'localhost:9092'
 
 if __name__ == '__main__':
-    consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=BOOTSTRAP_SERVERS)
+    ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    ssl_ctx.load_cert_chain(certfile="./cert.pem")
+    
+    consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=BOOTSTRAP_SERVERS, ssl_context=ssl_ctx)
     print '[+] Consuming topic "%s" from %s' % (TOPIC_NAME, BOOTSTRAP_SERVERS)
 
     for msg in consumer:
